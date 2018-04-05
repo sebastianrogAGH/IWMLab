221cdae97660710653581bb6baa87cf5d0426157
Sebastian Róg <rog@agh.edu.pl>
2018-04-05 04:06:44 +0200

    usuniete komentarze i dodanie testowanie domyslnego konstruktora

--- src/types/TypesTester.java
+++ src/types/TypesTester.java
@@ -2,18 +2,15 @@ package types;
 import types.Packet;
 
 public class TypesTester {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Packet testPack = new Packet("testDev","testDescr",100);
 		System.out.println(testPack);
 		Packet testPackDate = new Packet("testDev2","testDescr2",0);
 		System.out.println("date -> "+testPackDate.toString());
 		Packet testPackSimple = new Packet("simple");
 		System.out.println("simple -> "+testPackSimple.toString());
+		Packet testPackEmpty = new Packet();
+		System.out.println("empty -> "+testPackEmpty.toString());
 	}
 
 }
